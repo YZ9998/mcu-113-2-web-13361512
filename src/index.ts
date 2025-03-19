9addbb8d6f8a67c78e0fca5b6dc2feb4884f4b2f
yz,lin <jasmine950314@gmail.com>
2025-03-19 17:44:19 +0800

    feat(cs) 利用undefined 變數宣告產品建檔日期

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ let productName = "圖像 Angular 開發入門 第二版"; // 區域變數
 console.log("productName =", productName);
 let isShow = true;
 console.log("Is Show =", isShow);
+let createDate = undefined;
+console.log("Create Date =", createDate);
