96574c0e8c5bdfabffe1625c0f08435423b91b16
yz,lin <jasmine950314@gmail.com>
2025-03-19 18:34:56 +0800

    refactor(ts): 變更待產品建構式

--- src/product.ts
+++ src/product.ts
@@ -1,19 +1,11 @@
 export class Product {
-  constructor(id: number, name: string, compan: string, price: number, isShow: boolean, createDate: Date, modifyDate: undefined | Date) {
-    this.id = id;
-    this.name = name;
-    this.compan = compan;
-    this.price = price;
-    this.isShow = isShow;
-    this.createDate = createDate;
-    this.modifyDate = modifyDate;
-  }
-
-  id: number;
-  name: string;
-  compan: string;
-  price: number;
-  isShow: boolean;
-  createDate: Date;
-  modifyDate: undefined | Date;
+  constructor(
+    public id: number,
+    public name: string,
+    public compan: string,
+    public price: number,
+    public isShow: boolean,
+    public createDate: Date,
+    public modifyDate: undefined | Date
+  ) {}
 }
